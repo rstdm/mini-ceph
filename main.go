897dddcbe99d82e04192899b382f04cb2f215baf
Rene Maget <37067579+rstdm@users.noreply.github.com>
2022-11-22 17:05:02 +0100

    parse cli flags

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/rstdm/glados/internal/logger"
 	"os"
 )
 
 func main() {
-	log, loggerCleanup, err := logger.New(true)
+	useProductionLogger := true
+	flag.BoolVar(&useProductionLogger, "useProductionLogger", false, "Determines weather the logger should produce json output or human readable output")
+	flag.Parse()
+
+	log, loggerCleanup, err := logger.New(useProductionLogger)
 	if err != nil {
 		fmt.Printf("Failed to initialize logger: %v\n", err)
 		fmt.Println("Exiting.")
