395393f9cf515a50b17a1a9692aea9cbbd193bf7
Rene Maget <37067579+rstdm@users.noreply.github.com>
2022-11-28 18:19:34 +0100

    ensure that some values don't change between program starts

--- internal/configuration/configuration.go
+++ internal/configuration/configuration.go
@@ -2,19 +2,34 @@ package configuration
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"net/url"
+	"os"
+	"path/filepath"
+	"reflect"
 	"strconv"
+)
+
+const (
+	relativePersistedConfigurationPath = "persistedConfiguration.json"
+	relativeObjectStoragePath          = "data"
 )
 
 type Configuration struct {
 	UseProductionLogger bool
 	Port                int
 	BearerToken         string
-	ObjectFolder        string
 	MaxObjectSizeBytes  int64
 
+	ObjectFolder    string
+	NodeID          int
+	NodeHosts       map[int]string
+	PlacementGroups map[int][]int
+}
+
+type persistedConfiguration struct {
 	NodeID          int
 	NodeHosts       map[int]string
 	PlacementGroups map[int][]int
@@ -28,17 +43,18 @@ func Parse() (Configuration, error) {
 	flag.IntVar(&values.Port, "port", 5000, "Port on which to serve http requests.")
 	flag.StringVar(&values.BearerToken, "bearerToken", "", "BearerToken that is used to authorize all "+
 		"requests. Every request will be accepted without authorization if the token is empty.")
-	flag.StringVar(&values.ObjectFolder, "objectFolder", "./data", "Relative path to the folder that is "+
-		"used for object storage and retrieval")
 	flag.Int64Var(&values.MaxObjectSizeBytes, "maxObjectSizeBytes", 20000000, "Objects that are bigger than "+
 		"the specified size can not be persisted. Note that this doesn't influence already created objects which will "+
 		"still be available for download.")
 
 	// these values must be parsed / validated manually
+	var dataFolder string
 	var rawNodeID string // we cannot use an integer, because we have to detect absent values; the flag library doesn't support *int
 	var rawNodes string
 	var rawPlacementGroups string
 
+	flag.StringVar(&dataFolder, "dataFolder", ".", "Relative path to the folder that is "+
+		"used to store information.")
 	flag.StringVar(&rawNodeID, "nodeID", "", "non-negative integer which specifies the ID of the current node")
 	flag.StringVar(&rawNodes, "nodes", "", "json encoded key value map which maps the ID of each node "+
 		"to a host. The host must include the schema. Example: {\"0\": \"http://localhost:5000\"}")
@@ -48,31 +64,109 @@ func Parse() (Configuration, error) {
 
 	flag.Parse()
 
+	values.ObjectFolder = filepath.Join(dataFolder, relativeObjectStoragePath)
+	persistedConfigurationPath := filepath.Join(dataFolder, relativePersistedConfigurationPath)
+
+	pc, err := loadPersistedConfiguration(persistedConfigurationPath)
+	if err != nil {
+		err = fmt.Errorf("load persisted configuration: %w", err)
+		return Configuration{}, err
+	}
+
+	if pc != nil && rawNodeID == "" && rawNodes == "" && rawPlacementGroups == "" {
+		// an old instance persisted configuration data and the data hasn't been specified for the current instance
+		// -> use the old data
+		values.NodeID = pc.NodeID
+		values.NodeHosts = pc.NodeHosts
+		values.PlacementGroups = pc.PlacementGroups
+
+		return values, nil
+	}
+
+	// use the configuration data of this instance
+
 	nodeHosts, err := parseNodes(rawNodes)
 	if err != nil {
-		err = fmt.Errorf("parse network adresses of nodes (argument 2): %w", err)
+		err = fmt.Errorf("parse network adresses of nodes: %w", err)
 		return Configuration{}, err
 	}
 	values.NodeHosts = nodeHosts
 
 	nodeID, err := parseNodeID(err, rawNodeID, values.NodeHosts)
 	if err != nil {
-		err = fmt.Errorf("parse NodeID (argument 1): %w", err)
+		err = fmt.Errorf("parse NodeID: %w", err)
 		return Configuration{}, err
 	}
 	values.NodeID = nodeID
 
 	placementGroups, err := parsePlacementGroups(rawPlacementGroups, values.NodeHosts)
 	if err != nil {
-		err = fmt.Errorf("parse placement groups (argument 3): %w", err)
+		err = fmt.Errorf("parse placement groups: %w", err)
 		return Configuration{}, err
 	}
 	values.PlacementGroups = placementGroups
 
+	newPersistedConfiguration := persistedConfiguration{
+		NodeID:          values.NodeID,
+		NodeHosts:       values.NodeHosts,
+		PlacementGroups: values.PlacementGroups,
+	}
+
+	// make sure that the configuration for these fields hasn't changed
+	if pc != nil && !reflect.DeepEqual(*pc, newPersistedConfiguration) {
+		err = fmt.Errorf("immutable configuration values have changed from %#v to %#v", *pc, newPersistedConfiguration)
+		return Configuration{}, err
+	}
+
+	if pc == nil { // persist the configuration for the next instance
+		if err := persistConfiguration(newPersistedConfiguration, persistedConfigurationPath); err != nil {
+			err = fmt.Errorf("persist configuration: %w", err)
+			return Configuration{}, err
+		}
+	}
+
 	return values, nil
 }
 
+func loadPersistedConfiguration(persistedConfigurationPath string) (*persistedConfiguration, error) {
+	binaryContent, err := os.ReadFile(persistedConfigurationPath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil, nil
+		}
+
+		err = fmt.Errorf("open %v: %w", persistedConfigurationPath, err)
+		return nil, err
+	}
+
+	var pc persistedConfiguration
+	if err := json.Unmarshal(binaryContent, &pc); err != nil {
+		err = fmt.Errorf("parse content of %v: %w", persistedConfigurationPath, err)
+		return nil, err
+	}
+
+	return &pc, nil
+}
+
+func persistConfiguration(pc persistedConfiguration, persistedConfigurationPath string) error {
+	encodedData, err := json.Marshal(pc)
+	if err != nil {
+		return fmt.Errorf("json encode: %w", err)
+	}
+
+	if err := os.WriteFile(persistedConfigurationPath, encodedData, 0666); err != nil {
+		return fmt.Errorf("write to file %v: %w", persistedConfigurationPath, err)
+	}
+
+	return nil
+}
+
 func parseNodes(rawNodes string) (map[int]string, error) {
+	if rawNodes == "" {
+		err := errors.New("nodes must not be empty")
+		return nil, err
+	}
+
 	var parsedNodes map[int]string
 	if err := json.Unmarshal([]byte(rawNodes), &parsedNodes); err != nil {
 		return nil, fmt.Errorf("parse json '%v': %w", rawNodes, err)
@@ -100,6 +194,11 @@ func parseNodes(rawNodes string) (map[int]string, error) {
 }
 
 func parseNodeID(err error, rawNodeID string, nodes map[int]string) (int, error) {
+	if rawNodeID == "" {
+		err = errors.New("nodeID must not be empty")
+		return 0, err
+	}
+
 	nodeID, err := strconv.Atoi(rawNodeID)
 	if err != nil {
 		return 0, fmt.Errorf("rawNodeID '%v' is no valid integer: %w", rawNodeID, err)
@@ -116,6 +215,11 @@ func parseNodeID(err error, rawNodeID string, nodes map[int]string) (int, error)
 }
 
 func parsePlacementGroups(rawPlacementGroups string, nodes map[int]string) (map[int][]int, error) {
+	if rawPlacementGroups == "" {
+		err := errors.New("placementGroups must not be empty")
+		return nil, err
+	}
+
 	var parsedPlacementGroups map[int][]int
 	if err := json.Unmarshal([]byte(rawPlacementGroups), &parsedPlacementGroups); err != nil {
 		err = fmt.Errorf("parse json string %v: %w", rawPlacementGroups, err)
